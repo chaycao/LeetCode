898ce3fb2fadd585dc53941561ce497bfd4d5304
chaycao <429629038@qq.com>
2017-10-07 23:01:06 +0800

    140

--- 121-140/139/Solution_139.java
+++ 121-140/139/Solution_139.java
@@ -7,4 +7,9 @@ public class Solution_139 {
     public boolean wordBreak(String s, List<String> wordDict) {
 
     }
+    public boolean helper(String s, List<String> wordDict, int start) {
+        for (int i = start+1; i < s.length(); i++) {
+
+        }
+    }
 }
