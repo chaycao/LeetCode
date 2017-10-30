5bd84adda65508ac30d6e15f928d19c53a332d3a
chaycao <429629038@qq.com>
2017-10-30 09:40:07 +0800

    344

---
+++ 344/Solution_344.java
@@ -0,0 +1,9 @@
+/**
+ * Created by chaycao on 2017/10/30.
+ * chaycao@163.com
+ */
+public class Solution_344 {
+    public String reverseString(String s) {
+        return new StringBuilder(s).reverse().toString();
+    }
+}
