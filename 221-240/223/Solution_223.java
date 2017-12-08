77f4a00fc7439a6eb51584a22b86d70120b52e22
chaycao <429629038@qq.com>
2017-12-08 15:24:33 +0800

    223

---
+++ 221-240/223/Solution_223.java
@@ -0,0 +1,19 @@
+/**
+ * Created by chaycao on 2017/12/8.
+ * chaycao@163.com
+ */
+public class Solution_223 {
+    public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+        int areaA = (C-A)*(D-B);
+        int areaB = (G-E)*(H-F);
+        int left = Math.max(A,E);
+        int right = Math.min(C,G);
+        int bottom = Math.max(B,F);
+        int top = Math.min(D,H);
+        int overlap = 0;
+        if (top > bottom && right > left) {
+            overlap = (top - bottom) * (right - left);
+        }
+        return areaA + areaB - overlap;
+    }
+}
