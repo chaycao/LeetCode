3eb14b23414dfee3e4556a1fa758e8c6efc7bd4a
chaycao <429629038@qq.com>
2018-05-18 22:52:59 +0800

    292

---
+++ 292/Solution_292.java
@@ -0,0 +1,12 @@
+/**
+ * @author chaycao
+ * @description
+ * @date 2018-05-18 22:49.
+ */
+public class Solution_292 {
+    public boolean canWinNim(int n) {
+        if (n%4 == 0)
+            return false;
+        return true;
+    }
+}
